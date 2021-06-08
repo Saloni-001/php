2e8e57bfa8cf342cbf7781afde41e0db6690c9cc
Saloni-001 <84764292+Saloni-001@users.noreply.github.com>
2021-06-08 13:19:32 +0530

    array

---
+++ array.php
@@ -0,0 +1,19 @@
+<?php
+
+$contacts = array(
+    array(
+        "name" => "Peter Parker",
+        "email" => "peterparker@mail.com",
+    ),
+    array(
+        "name" => "Clark Kent",
+        "email" => "clarkkent@mail.com",
+    ),
+    array(
+        "name" => "Harry Potter",
+        "email" => "harrypotter@mail.com",
+    )
+);
+
+echo "Peter Parker's Email-id is: " . $contacts[0]["email"];
+?>
