ec8c14947e6e09ea035f97adfa577b1fd8940016
Saloni-001 <84764292+Saloni-001@users.noreply.github.com>
2021-06-08 13:22:17 +0530

    math

---
+++ math.php
@@ -0,0 +1,9 @@
+<?php
+echo 5 + 4 * 10;         // 0utputs: 45
+echo (5 + 4) * 10;       // 0utputs: 90
+echo 5 + 4 * 10 / 2;     // 0utputs: 25
+echo 8 * 10 / 4 - 2;     // 0utputs: 18
+echo 8 * 10 / (4 - 2);   // 0utputs: 40
+echo 8 + 10 / 4 - 2;     // 0utputs: 8.5
+echo (8 + 10) / (4 - 2); // 0utputs: 9
+?>
