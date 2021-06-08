c629dfc72fef177551c9ffdfa9550c858d74ceb2
Saloni-001 <84764292+Saloni-001@users.noreply.github.com>
2021-06-08 13:19:52 +0530

    associate

---
+++ associate.php
@@ -0,0 +1,23 @@
+<html>
+   <body>
+      
+      <?php
+         
+         $salaries = array("mohammad" => 2000, "qadir" => 1000, "zara" => 500);
+         
+         echo "Salary of mohammad is ". $salaries['mohammad'] . "<br />";
+         echo "Salary of qadir is ".  $salaries['qadir']. "<br />";
+         echo "Salary of zara is ".  $salaries['zara']. "<br />";
+         
+        
+         $salaries['mohammad'] = "high";
+         $salaries['qadir'] = "medium";
+         $salaries['zara'] = "low";
+         
+         echo "Salary of mohammad is ". $salaries['mohammad'] . "<br />";
+         echo "Salary of qadir is ".  $salaries['qadir']. "<br />";
+         echo "Salary of zara is ".  $salaries['zara']. "<br />";
+      ?>
+   
+   </body>
+</html>
